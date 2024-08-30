e3225030252c68e98433bb33311aa61ba6df39da
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-08-30 23:48:03 +0100

    cleaned up day2

--- 2021/day_2.py
+++ 2021/day_2.py
@@ -1,90 +1,72 @@
-'''Solution to day 2'''
+'''Solution to day 2.
+
+Each instruction of the form [direction] [X] represents a change in orientation and a translation of the submarine in the direction of the new orientation.
+
+For example, in the one star case, "forward" flips the orientation to (1,0), "up" and "down" switch the orientation to (0,-1) and (0,1). The submarine then changes it's position by X units in the new orientation direction.
+
+In the two star case, the new orientation after an instruction depends on a separate "aim" parameter. Suppose we start with aim x; "forward y" will change the orientation to (1,x). The submarine is translated y units in this direction, so its position changes by y * (1,x) points.
+
+'''
+
+import numpy as np
 
 FILE = "inputs/day_2_input.txt"
-DIRECTIONS = {
-    "forward": [1, 0],
-    "up": [0, -1],
-    "down": [0, 1]
-}
+TEST_FILE = "inputs/day_2_test_input.txt"
 
 
-def get_net_directions_count(filename: str) -> list[str]:
-    net_directions = {"forward": 0, "up": 0, "down": 0}
-    with open(filename, "r") as f:
-        while True:
-            line = f.readline()
-            if not line:
-                break
-            key, val = line.replace("\n", "").strip().split()
+def read_input_file(filename: str) -> list[list]:
+    '''Read the input file and return a list of instructions.'''
 
-            if net_directions.get(key.strip()) is not False:
-                net_directions[key.strip()] += int(val)
-    return net_directions
-
-
-def add_vectors(v1, v2):
-    if len(v1) == len(v2):
-        return [v1[i] + v2[i] for i in range(len(v1))]
-
-
-def multiply_scalar(s, v):
-    return [s*v_i for v_i in v]
-
-
-def find_net_direction(counts: dict):
-    net = [0, 0]
-
-    for direction, count in counts.items():
-        orientation = DIRECTIONS[direction]
-        net = add_vectors(net, multiply_scalar(count, orientation))
-
-    return net
-
-# TODO sep load, process and get answer functions
-
-
-def find_net_direction_with_aim(filename: str) -> list[int, int]:
-
-    increments = {
-        "forward": lambda curr: [1, curr[2], 0],
-        "up": lambda curr: [0, 0, -1],
-        "down": lambda curr: [0, 0, 1]
-    }
-
-    net = [0, 0, 0]  # horizontal,depth, aim
-    with open(filename, "r") as f:
+    instructions = []
+    with open(filename, 'r') as f:
         for line in f.readlines():
+            direction, amount = line.replace("\n", "").strip().split(" ")
+            instructions.append([direction, int(amount)])
 
-            key, val = line.replace("\n", "").strip().split()
-
-            next_increment = multiply_scalar(int(val), increments[key](net))
-
-            print(next_increment)
-
-            net = add_vectors(net, next_increment)
-            print(f"net = {net}")
-        return net
+    return instructions
 
 
-def find_net_dir_with_aim_2(filename: str):
-    net = {"horizontal": 0, "depth": 0, "aim": 0}
-    with open(filename, "r") as f:
-        for line in f.readlines():
-            key, val = line.replace("\n", "").strip().split()
-            val = int(val)
-            if key == "forward":
-                net["horizontal"] += val
-                net["depth"] += net["aim"] * val
-            elif key == "up":
-                net["aim"] -= val
-            else:
-                net["aim"] += val
-    return net
+def map_direction_to_new_orientation_one_star(direction_name: str):
+    directions = {"forward": np.array([1, 0]),
+                  "up": np.array([0, -1]),
+                  "down": np.array([0, 1])}
+    return directions[direction_name]
+
+
+def map_direction_to_new_orientation_two_star(direction_name: str, current_aim: int):
+    return np.array([1, current_aim]) if direction_name == "forward" else np.array([0, 0])
+
+
+def get_aim_increment(direction_name: str, distance: int) -> int:
+    '''Returns the change in aim after a particular instruction.'''
+    directions = {"forward": 0, "up": -1, "down": 1}
+    return directions[direction_name]*distance
+
+
+def get_final_position_one_star(instructions: list[list]):
+    '''Each instruction is a list of the form [direction,distance].
+    Given a list of such instructions, find the final position of the submarine.'''
+
+    return sum([i[1] * map_direction_to_new_orientation_one_star(i[0])
+                for i in instructions])
+
+
+def get_final_position_two_stars(instructions: list[list]):
+    '''Returns the final position for the two star problem.'''
+    current_aim = 0
+    current_pos = np.array([0, 0])
+    for i in instructions:
+        current_pos = sum([current_pos, i[1] *
+                           map_direction_to_new_orientation_two_star(i[0], current_aim)])
+        current_aim += get_aim_increment(i[0], i[1])
+    return current_pos
 
 
 if __name__ == "__main__":
-    c = get_net_directions_count(FILE)
 
-    print(find_net_direction_with_aim(FILE))
-    net2 = find_net_dir_with_aim_2(FILE)
-    print(net2)
+    instructions = read_input_file(FILE)
+    final_pos = get_final_position_one_star(instructions)
+    print(f"One star solution: {final_pos[0]*final_pos[1]}")
+
+    new_final_pos = get_final_position_two_stars(instructions)
+    print(f"Two star solution: {new_final_pos[0]*new_final_pos[1]}")
