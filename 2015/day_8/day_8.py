17d66e38450d9e6d092b426f2dff449d278eb716
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-09-17 13:58:32 +0100

    added directory for day 8

---
+++ 2015/day_8/day_8.py
@@ -0,0 +1,34 @@
+'''Solution to advent of code day 10 2021
+'''
+
+INPUT_FILE = "inputs/day_10_input.txt"
+TEST_FILE = "inputs/day_10_test_input.txt"
+
+
+def load_file(filename: str) -> list[int]:
+    '''Loads the file as a list of integers'''
+
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    return [l.replace("\n", "").replace(" ", "") for l in lines]
+
+
+def one_star(filename: str):
+    '''Returns the one star solution'''
+    lines = load_file(filename)
+
+    return
+
+
+def two_star(filename: str):
+    '''Returns the two star solution'''
+
+    return
+
+
+if __name__ == "__main__":
+    print(f"One star solution is {one_star(TEST_FILE)}")
+    print(f"Two star solution is {two_star(TEST_FILE)}")
+    print(f"One star solution is {one_star(INPUT_FILE)}")
+    print(f"Two star solution is {two_star(INPUT_FILE)}")
