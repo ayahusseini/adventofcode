b1243dbd9db4276c9f89d0c3cef7b62b3d23f36c
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-09-22 02:00:02 +0100

    fin

--- 2015/day_11/day_11.py
+++ 2015/day_11/day_11.py
@@ -1,23 +1,91 @@
 '''Solution to advent of code day 11 2015
 '''
-
-
 INPUT = "hxbxwxba"
+TEST = "abcdefgh"
+
+
+def get_alphabet() -> str:
+    '''Return the alphabet'''
+    return "abcdefghijklmnopqrstuvwxyz"
+
+
+def get_next_letter(s: str) -> str:
+    '''Returns the next letter in the alphabet, or "a" if s == z'''
+
+    s = s.lower()
+    alphabet = get_alphabet()
+    return alphabet[alphabet.find(s)+1] if s != "z" else "a"
+
+
+def increment_once(seq: str) -> str:
+    '''Increment the last letter in the sequence.
+    Increment the letter to it's left if it wraps around, 
+    continuing until one doesn't wrap.'''
+
+    for i, letter in enumerate(seq[::-1]):
+        inc = get_next_letter(letter)
+        if inc != "a":
+            return seq[:-i-1] + inc + "a"*i
+    return "a"*len(seq)
+
+
+def count_subsequences(seq: str, length: int, overlapping: bool, rule) -> str:
+    '''Return the number of subsequences of a given length 
+    within seq which satisfy some rule'''
+
+    i = 0
+    count = 0
+    while i <= len(seq) - length:
+        is_valid_subseq = rule(seq[i:i+length])
+        if is_valid_subseq:
+            count += 1
+            if not overlapping:
+                i += length
+            else:
+                i += 1
+        else:
+            i += 1
+
+    return count
+
+
+def is_increasing_alphabetically(text: str) -> bool:
+    '''Returns True if letters in text are increasing alphabetically.'''
+    return text in get_alphabet()
+
+
+def is_matching(text: str) -> bool:
+    '''Returns True if all letters in the text are the same'''
+    if not text:
+        return True
+    return text == text[0] * len(text)
+
+
+def does_not_contain(text: str, cannot_contain: list[str] = ["i", "o", "l"]) -> bool:
+    '''Returns True if text doesn't contain cannot_contain'''
+    return set(text) - set(cannot_contain) == set(text)
+
+
+def is_valid_password(seq: str) -> bool:
+    '''Returns True if a password is valid'''
+
+    return does_not_contain(seq, ["i", "o", "l"]) and (count_subsequences(seq, 2, False, is_matching) > 1) and (count_subsequences(seq, 3, True, is_increasing_alphabetically) > 0)
 
 
 def one_star(input_sequence: str):
     '''Returns the one star solution'''
-
-    return
+    while not is_valid_password(input_sequence):
+        input_sequence = increment_once(input_sequence)
+    return input_sequence
 
 
 def two_star(input_sequence: str):
     '''Returns the two star solution'''
-
-    return
+    input_sequence = increment_once(one_star(input_sequence))
+    return one_star(input_sequence)
 
 
 if __name__ == "__main__":
-
+    print(f"One star test solution is {one_star(TEST)}")
     print(f"One star solution is {one_star(INPUT)}")
     print(f"Two star solution is {two_star(INPUT)}")
--- 2015/day_11/test_day_11.py
+++ 2015/day_11/test_day_11.py
@@ -1,21 +1,114 @@
-from day_10 import transform_string
+import pytest
+from day_11 import increment_once, is_increasing_alphabetically, count_subsequences, is_matching, does_not_contain, is_valid_password
 
 
-def test_transform():
-    assert transform_string("1") == "11"
+@pytest.mark.parametrize(
+    "seq,exp",
+    [("hijklmmn", False),
+     ("abbceffg", False),
+     ("abbcegjk", False),
+     ("abcdffaa", True)])
+def test_valid_pw(seq, exp):
+    assert is_valid_password(seq) == exp
 
 
-def test_transform2():
-    assert transform_string("11") == "21"
+def test_does_not_contain():
+    assert does_not_contain("abbceffg")
 
 
-def test_transform3():
-    assert transform_string("21") == "1211"
+def test_does_not_contain_again():
+    assert does_not_contain("abcdffaa")
 
 
-def test_transform4():
-    assert transform_string("1211") == "111221"
+def test_does_not_contain_empty():
+    assert does_not_contain("")
 
 
-def test_transform5():
-    assert transform_string("111221") == "312211"
+def test_does_contain():
+    assert not does_not_contain("abbceffgi")
+
+
+def test_does_contain2():
+    assert not does_not_contain("hijklmmn")
+
+
+def test_matching():
+    assert is_matching("aa")
+
+
+def test_matching_empty():
+    assert is_matching("")
+
+
+def test_not_matching():
+    assert not is_matching("aabc")
+
+
+def test_alphabetic_increase():
+    assert is_increasing_alphabetically("abc")
+
+
+def test_alphabetic_increase2():
+    assert is_increasing_alphabetically("xyz")
+
+
+def test_not_alphabetic_increase1():
+    assert not is_increasing_alphabetically("bdf")
+
+
+def test_not_alphabetic_increase2():
+    assert not is_increasing_alphabetically("yza")
+
+
+def test_increment_once():
+    assert increment_once("xx") == "xy"
+
+
+def test_increment_once2():
+    assert increment_once("xy") == "xz"
+
+
+def test_increment_once3():
+    assert increment_once("xz") == "ya"
+
+
+def test_increment_once4():
+    assert increment_once("ya") == "yb"
+
+
+def test_increment_once_long_string():
+    assert increment_once("zzzzz") == "a"*5
+
+
+def test_increment_once_long_string2():
+    assert increment_once("azzzzz") == "b"+"a"*5
+
+
+def test_basic_count_subsequences_not_overlapping():
+    assert count_subsequences(
+        "aabbcc", 2, False, lambda x: x.count(x[0]) == len(x)) == 3
+
+
+def test_basic_count_subsequences_not_overlapping2():
+    assert count_subsequences(
+        "aaabbcc", 2, False, lambda x: x.count(x[0]) == len(x)) == 3
+
+
+def test_basic_count_subsequences_overlapping():
+    assert count_subsequences(
+        "aaa", 2, True, lambda x: x.count(x[0]) == len(x)) == 2
+
+
+def test_basic_count_subsequences_overlapping2():
+    assert count_subsequences(
+        "aaa", 1, True, lambda x: True) == 3
+
+
+def test_basic_count_subsequences_not_overlapping3():
+    assert count_subsequences(
+        "aaa", 1, True, lambda x: True) == 3
+
+
+def test_basic_count_subsequences_overlapping3():
+    assert count_subsequences(
+        "aaabbcc", 2, True, lambda x: x.count(x[0]) == len(x)) == 4
