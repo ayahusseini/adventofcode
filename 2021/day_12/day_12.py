de2cc37232e94fcd98431026c0a0bb60b16f4776
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-21 17:46:04 +0100

    finished one star

--- 2021/day_12/day_12.py
+++ 2021/day_12/day_12.py
@@ -1,39 +1,87 @@
 '''Solution to day 12 2021'''
-
 from collections import defaultdict
+INPUT_FILE = "inputs/day_12_input.txt"
+TEST_FILE = "inputs/day_12_test_input.txt"
 
-
-class CaveMap(object):
-    '''Represents the nodes and paths on the map.'''
-
-    def __init__(self):
-        # maps nodes to the set of nodes they are directly connected to
+class Map:
+    """A map of the cave system"""
+    def __init__(self,
+                 cave_connections:list[tuple[str,str]],
+                 start: str, 
+                 end: str):
+        '''Instantiates a map of caves'''
+        
+        self.start = start 
+        self.end = end 
+        
         self.connections = defaultdict(lambda: set())
-        self.visit_once_max = set()
 
-    def add_edge(self, cave_1: str, cave_2: str):
-        '''Updates the connections'''
-        self.connections[cave_1].update([cave_2])
-        self.connections[cave_2].update([cave_1])
-        if cave_1.islower():
-            self.visit_once_max.update([cave_1])
-        if cave_2.islower():
-            self.visit_once_max.update([cave_2])
+        for connection in cave_connections:
+            cave1, cave2 = connection
+            self.connections[cave1].update([cave2])
+            self.connections[cave2].update([cave1])
 
-    def find_cave_path(self, start: str, end: str):
-        '''Find all cave paths from the start to the end'''
+    @classmethod 
+    def from_file_lines(cls, 
+                        lines:list[str],
+                        start_name:str, 
+                        end_name:str):
+        """Instantiate a map from a list of filelines"""
+        connections = []
+        for line in lines:
+            line = line.replace("\n","").strip()
+            
+            connection = [cave for cave in line.split('-')]
+            connections.append(tuple(connection))
+
+        return cls(connections,start_name, end_name)
+    
+    def get_paths(self):
+        """Return all paths from start to end"""
         paths = []
+        stack = [(self.start,[self.start])]
 
-        curr_path = [start]
-        visited = []
-        # remove the first node to get the nearest neighbour to A
-        neighbours = [start]
+        while stack: 
 
-        while curr_path[-1] != end:
-            next_cave = neighbours.pop()
+            cur_source, cur_path = stack.pop()
 
-            if next_cave not in visited
+            if cur_source == self.end:
+                paths += [cur_path]
+
+            else:
+                for neighbour in self.connections[cur_source]:
+                    if not (neighbour.islower() and neighbour in cur_path):
+                        stack += [(neighbour, cur_path + [neighbour])]
+
+        return paths 
+    
+
+def load_file(filename: str) -> list[str]:
+    '''Loads the file as a list of strings'''
+
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    return lines
+
+
+def one_star(filename: str):
+    '''Returns the one star solution'''
+    lines = load_file(filename)
+    map = Map.from_file_lines(lines,"start","end")
+
+    return len(map.get_paths())
+
+
+def two_star(filename: str):
+    '''Returns the two star solution'''
+    lines = load_file(filename)
+    return 
+
 
 
 if __name__ == "__main__":
-    ...
+    print(f"One star solution is {one_star(TEST_FILE)}")
+    #print(f"Two star solution is {two_star(TEST_FILE)}")
+    print(f"One star solution is {one_star(INPUT_FILE)}")
+    print(f"Two star solution is {two_star(INPUT_FILE)}")
