3438029e7b487c337c85529595f72f76d7004ec9
ayahusseini <aya.ahusseini@gmail.com>
2024-12-01 16:50:34 +0000

    made changes

--- 2024/day_1/day_1.py
+++ 2024/day_1/day_1.py
@@ -14,15 +14,19 @@ def load_file(filename: str) -> list[int]:
         for line in lines:
             ints = line.split("   ")
             l1.append(int(ints[0]))
-            l2.append(int(ints[0]))
+            l2.append(int(ints[1]))
     return l1, l2 
 
 
 def one_star(filename: str):
     '''Returns the one star solution'''
-    lines = load_file(filename)
+    l1, l2 = load_file(filename)
+    print(l1)
+    print(l2)
+    l1.sort()
+    l2.sort()
 
-    return
+    return sum([abs(l1[i] - l2[i]) for i in range(len(l1))])
 
 
 def two_star(filename: str):
@@ -32,7 +36,7 @@ def two_star(filename: str):
 
 
 if __name__ == "__main__":
-    print(f"One star solution is {one_star(TEST_FILE)}")
-    print(f"Two star solution is {two_star(TEST_FILE)}")
-    print(f"One star solution is {one_star(INPUT_FILE)}")
+    print(f"One star test solution is {one_star(TEST_FILE)}")
+    #print(f"Two star test solution is {two_star(TEST_FILE)}")
+   #print(f"One star solution is {one_star(INPUT_FILE)}")
     print(f"Two star solution is {two_star(INPUT_FILE)}")
