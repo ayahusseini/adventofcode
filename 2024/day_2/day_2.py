5a2bbf5251b7e7b7b0ffef1aee08fb4f6c7dec0e
ayahusseini <aya.ahusseini@gmail.com>
2024-12-02 15:37:54 +0000

    added one star solution

--- 2024/day_2/day_2.py
+++ 2024/day_2/day_2.py
@@ -6,14 +6,51 @@ INPUT_FILE = "inputs/day_2_input.txt"
 TEST_FILE = "inputs/day_2_test_input.txt"
 
 
-def load_file(filename: str) -> list[int]:
+class Report:
+    def __init__(self, numbers: list):
+        """Instantiates a report object"""
+        self.numbers = numbers
+
+    @classmethod
+    def from_line(cls, line: str):
+        """Instantiates a report from a line"""
+        return cls(list(map(lambda x: int(x), line.split(' '))))
+
+    def is_safe(self) -> bool:
+        """Returns True if a report is safe"""
+        is_increasing = None
+
+        for dif in self.get_differences():
+            if is_increasing is None:
+                is_increasing = dif > 0
+            else:
+                if is_increasing is not (dif > 0):
+                    return False
+            if abs(dif) not in range(1, 4):
+                return False
+        return True
+
+    def get_differences(self):
+        """Returns the differences between adjacent pairs"""
+        differences = []
+        for i in range(0, len(self.numbers)-1):
+            yield self.numbers[i] - self.numbers[i+1]
+
+
+def load_file(filename: str) -> list[Report]:
     '''Loads the file as two lists of integers'''
+    reports = []
     with open(filename, "r") as f:
         lines = f.readlines()
+        for line in lines:
+            reports.append(Report.from_line(line))
+    return reports
 
 
-def one_star(filename: str):
+def one_star(filename: str) -> int:
     '''Returns the one star solution'''
+    reports = load_file(filename)
+    return sum([r.is_safe() for r in reports])
 
 
 def two_star(filename: str):
