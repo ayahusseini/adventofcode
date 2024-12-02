4b55c7116adc2a9cedc077a01e556f8dc644617d
ayahusseini <aya.ahusseini@gmail.com>
2024-12-02 16:22:08 +0000

    some commits

--- 2024/day_2/day_2.py
+++ 2024/day_2/day_2.py
@@ -16,17 +16,20 @@ class Report:
         """Instantiates a report from a line"""
         return cls(list(map(lambda x: int(x), line.split(' '))))
 
-    def is_safe(self) -> bool:
+    def is_safe(self, num_removals: int = 0) -> bool:
         """Returns True if a report is safe"""
         is_increasing = None
+        invalid_count = 0
 
         for dif in self.get_differences():
             if is_increasing is None:
                 is_increasing = dif > 0
             else:
                 if is_increasing is not (dif > 0):
-                    return False
+                    invalid_count += 1
             if abs(dif) not in range(1, 4):
+                invalid_count += 1
+            if invalid_count > num_removals:
                 return False
         return True
 
@@ -55,6 +58,8 @@ def one_star(filename: str) -> int:
 
 def two_star(filename: str):
     '''Returns the two star solution'''
+    reports = load_file(filename)
+    return sum([r.is_safe(num_removals=1) for r in reports])
 
 
 if __name__ == "__main__":
