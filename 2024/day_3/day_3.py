786d4e352ce11c3382cc4e06096065daceeb89d7
ayahusseini <aya.ahusseini@gmail.com>
2024-12-03 18:08:01 +0000

    added second test file

--- 2024/day_3/day_3.py
+++ 2024/day_3/day_3.py
@@ -1,35 +1,59 @@
-'''Solution to advent of code day 4 2024
+'''Solution to advent of code day 3 2024
 '''
 from collections import defaultdict
 import re
 
-INPUT_FILE = "inputs/day_4_input.txt"
-TEST_FILE = "inputs/day_4_test_input.txt"
+INPUT_FILE = "inputs/day_3_input.txt"
+TEST_FILE = "inputs/day_3_test_input.txt"
+TEST_FILE2 = "inputs/day_3_test_input2.txt"
+
+MULTIPLICATION_PATTERN = r"mul\((\d+),(\d+)\)"
+CONDITIONAL_MULTIPLICATION_PATTERN = r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))"
 
 
-def extract_multiplication_instructions(memory: str) -> list[tuple[str, str]]:
+def extract_multiplication_instructions(memory: str, pattern: str) -> list[tuple]:
     """Returns a list of multiplication instructions as a list of tuples
     to multiply"""
-    return re.findall(r"mul\((\d+),(\d+)\)", memory)
+    return re.findall(pattern, memory)
 
 
-def load_file(filename: str) -> list[int]:
+def read_conditional_instructions(instructions: list[tuple]) -> int:
+    """Reads the conditional instructions"""
+    multiplier = 1
+    total = 0
+    for match in instructions:
+        if any(match):
+            if match[0] and match[1]:
+                total += multiplier * int(match[0]) * int(match[1])
+            elif match[2]:
+                multiplier = 1
+            elif match[3]:
+                multiplier = 0
+    return total
+
+
+def load_file(filename: str, extraction_pattern: str = r"mul\((\d+),(\d+)\)") -> list[int]:
     '''Loads the file and returns a list of multiplication instructions'''
     with open(filename, 'r') as f:
-        instructions = extract_multiplication_instructions(f.read())
+        instructions = extract_multiplication_instructions(
+            f.read(), extraction_pattern)
     return instructions
 
 
 def one_star(filename: str):
     '''Returns the one star solution'''
+    instructions = load_file(filename)
+    return sum([int(x)*int(y) for x, y in instructions])
 
 
 def two_star(filename: str):
     '''Returns the two star solution'''
+    instructions = load_file(filename)
+    return read_conditional_instructions(instructions)
 
 
 if __name__ == "__main__":
     print(f"One star test solution is {one_star(TEST_FILE)}")
-    print(f"Two star test solution is {two_star(TEST_FILE)}")
+    print(f"Two star test solution is {two_star(TEST_FILE2)}")
     print(f"One star solution is {one_star(INPUT_FILE)}")
     print(f"Two star solution is {two_star(INPUT_FILE)}")
--- 2024/day_3/test_day_3.py
+++ 2024/day_3/test_day_3.py
@@ -1,5 +1,10 @@
 import pytest
-from day_3 import extract_multiplication_instructions
+from day_3 import extract_multiplication_instructions, read_conditional_instructions, MULTIPLICATION_PATTERN, CONDITIONAL_MULTIPLICATION_PATTERN
+
+
+@pytest.fixture
+def conditional_line():
+    return "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 
 @pytest.mark.parametrize("line,instructions", [
@@ -9,4 +14,11 @@ from day_3 import extract_multiplication_instructions
     ("mul(11,20)", [('11', '20')])
 ])
 def test_find_mul_instructions(line, instructions):
-    assert extract_multiplication_instructions(line) == instructions
+    assert extract_multiplication_instructions(
+        line, MULTIPLICATION_PATTERN) == instructions
+
+
+def test_conditional_instructions(conditional_line):
+    instructions = extract_multiplication_instructions(
+        conditional_line, CONDITIONAL_MULTIPLICATION_PATTERN)
+    assert read_conditional_instructions(instructions) == 48
