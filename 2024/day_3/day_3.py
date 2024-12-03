aabeefaa1c6fb90af5e3d03de33cdffbf2827a78
ayahusseini <aya.ahusseini@gmail.com>
2024-12-03 09:55:11 +0000

    added scaffolding for day 3

---
+++ 2024/day_3/day_3.py
@@ -0,0 +1,25 @@
+'''Solution to advent of code day 4 2024
+'''
+from collections import defaultdict
+
+INPUT_FILE = "inputs/day_4_input.txt"
+TEST_FILE = "inputs/day_4_test_input.txt"
+
+
+def load_file(filename: str) -> list[int]:
+    '''Loads the file'''
+
+
+def one_star(filename: str):
+    '''Returns the one star solution'''
+
+
+def two_star(filename: str):
+    '''Returns the two star solution'''
+
+
+if __name__ == "__main__":
+    print(f"One star test solution is {one_star(TEST_FILE)}")
+    print(f"Two star test solution is {two_star(TEST_FILE)}")
+    print(f"One star solution is {one_star(INPUT_FILE)}")
+    print(f"Two star solution is {two_star(INPUT_FILE)}")
