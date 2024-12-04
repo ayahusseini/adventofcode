e590a62e5ee9950ca25b6b9de5639438b52fbaf6
ayahusseini <aya.ahusseini@gmail.com>
2024-12-04 18:50:04 +0000

    added input and fixed input formatting

--- 2024/day_4/day_4.py
+++ 2024/day_4/day_4.py
@@ -1,16 +1,39 @@
 '''Solution to advent of code day 4 2024
 '''
-from collections import defaultdict
+import numpy as np
+from collections import defaultdict, namedtuple
 
 INPUT_FILE = "inputs/day_4_input.txt"
 TEST_FILE = "inputs/day_4_test_input.txt"
 
 
-def load_file(filename: str) -> list:
+Word = namedtuple('Word', ['orientation', 'word', 'is_backwards'])
+
+
+class WordSearch:
+    """Word search class"""
+
+    def __init__(self, lines: list[list]):
+        """Instantiates a word search object"""
+        self.lines = np.array(lines)
+        self.nrows = self.lines.shape[0]
+
+    def count_target(self, target) -> int:
+        """Finds the number of times a target word appears in lines"""
+        # find the first letter of the target word
+        start_letter = np.where(self.lines == target[0])
+        stack = [np.unravel_index(s,)]
+
+    def __str__(self) -> str:
+        """Returns a string representation of the lines"""
+
+
+def load_file(filename: str) -> WordSearch:
     '''Loads the file'''
     with open(filename, "r") as f:
         lines = f.readlines()
-    return lines
+    print(lines)
+    return WordSearch([list(l.replace('\n', '')) for l in lines if l])
 
 
 def one_star(filename: str):
@@ -25,7 +48,9 @@ def two_star(filename: str):
 
 
 if __name__ == "__main__":
-    print(f"One star test solution is {one_star(TEST_FILE)}")
-    print(f"Two star test solution is {two_star(TEST_FILE)}")
-    print(f"One star solution is {one_star(INPUT_FILE)}")
-    print(f"Two star solution is {two_star(INPUT_FILE)}")
+    ws = load_file(TEST_FILE)
+    print(str(ws))
+    # print(f"One star test solution is {one_star(TEST_FILE)}")
+    # print(f"Two star test solution is {two_star(TEST_FILE)}")
+    # print(f"One star solution is {one_star(INPUT_FILE)}")
+    # print(f"Two star solution is {two_star(INPUT_FILE)}")
