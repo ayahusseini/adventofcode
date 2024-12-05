1866351150f981843656b614fea95971a55b4b78
ayahusseini <aya.ahusseini@gmail.com>
2024-12-05 14:32:50 +0000

    started rewriting tests

--- 2024/day_5/test_day_5.py
+++ 2024/day_5/test_day_5.py
@@ -5,14 +5,12 @@ from day_5 import Page, RuleSet, TEST_FILE
 @pytest.fixture
 def rs():
     rules = RuleSet()
-    rules.add_rule(Page('75'), Page('47'))
-    rules.add_rule(Page('75'), Page('9'))
+    rules.add_rule(7, 5)
+    rules.add_rule(7, 47)
+    rules.add_rule(5, 47)
     return rules
 
 
-def test_overlapping_rules(rs):
-    assert rs.pages['75'].next == {rs.pages['47'], rs.pages['9']}
-
-
-def test_sorting(rs):
-    assert rs.is_sorted(['75', '47', '9'])
+def test_pages_added(rs):
+    assert set(7, 5, 47) in rs.pages
+    assert rs[47].next == set()
