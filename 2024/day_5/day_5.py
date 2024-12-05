d133adc909b3d07190e3a1775dff2992a7aaaed7
ayahusseini <aya.ahusseini@gmail.com>
2024-12-05 11:01:36 +0000

    added test to check if sorted

--- 2024/day_5/day_5.py
+++ 2024/day_5/day_5.py
@@ -5,6 +5,47 @@ INPUT_FILE = "inputs/day_5_input.txt"
 TEST_FILE = "inputs/day_5_test_input.txt"
 
 
+class Page:
+    def __init__(self, num: str):
+        self.page_num = num
+        self.prev = set()
+        self.next = set()
+
+    def __gt__(self, page2):
+        return (page2 in self.prev) or not (page2 in self.next)
+
+    def __str__(self):
+        return str(self.page_num)
+
+
+class RuleSet:
+    def __init__(self):
+        self.pages = dict()
+
+    def add_rule(self, before: Page, after: Page):
+        """Adds a rule"""
+        before.next.add(after)
+        after.prev.add(before)
+
+        if before in self.pages:
+            for page in before.prev:
+                page.next.add(after)
+
+        if after in self.pages:
+            for page in after.next:
+                page.prev.add(before)
+
+        self.pages[after.page_num] = after
+        self.pages[before.page_num] = before
+
+    def is_sorted(self, pages: list[int]):
+        """Returns True if a list of page numbers is sorted in increasing order."""
+        for i in range(0, len(pages) - 1):
+            p1, p2 = pages[i:i+2]
+            if self.pages[p1] > self.pages[p2]:
+                return False
+
+
 def load_file(filename: str):
     '''Loads the file'''
 
