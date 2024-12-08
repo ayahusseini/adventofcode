31026fd8398796c3772851b0a162690f00db0b7b
ayahusseini <aya.ahusseini@gmail.com>
2024-12-08 19:15:19 +0000

    added input

--- 2024/day_8/day_8.py
+++ 2024/day_8/day_8.py
@@ -1,9 +1,9 @@
 """Solution for advent of code day 8 2024"""
-from collections import namedtuple
+
+from collections import defaultdict
+
 INPUT_FILE = "inputs/day_8_input.txt"
 TEST_FILE = "inputs/day_8_test_input.txt"
-
-Node = namedtuple("node", ("symbol", "pos"))
 
 
 def multiply(scalar: int, vector: tuple, ) -> tuple:
@@ -25,11 +25,10 @@ def is_in_bounds(position: tuple, numrows: int, numcols: int) -> bool:
     return (0 <= position[0] < numrows) and (0 <= position[1] < numcols)
 
 
-def get_antinodes(n1: Node, n2: Node, nrows: int, ncols: int):
-    """Generates a pair of antinodes"""
-    if not n1.symbol == n2.symbol:
-        raise ValueError("They must have the same symbol.")
+def get_antinodes(antennas: list[tuple], nrows: int, ncols: int):
+    """Generates antinodes given a list of antennas with the same symbol"""
 
+    for i, antennna in antennas
     positions = get_antinode_positions(n1.pos, n2.pos)
 
     for pos in positions:
@@ -37,11 +36,15 @@ def get_antinodes(n1: Node, n2: Node, nrows: int, ncols: int):
             yield Node(symbol='#', pos=pos)
 
 
-def load_file(filename) -> list[str]:
+def load_file(filename) -> dict[Node]:
     """Loads the file"""
+    frequencies = defa
     with open(filename, 'r') as f:
         lines = f.readlines()
     return lines
+
+
+def calculate_antinodes(filename)
 
 
 def one_star(filename: str):
