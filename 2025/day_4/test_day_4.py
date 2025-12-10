624ef6fc5dcaf08d66efcd0765d42ebad4277024
ayahusseini <aya.ahusseini@gmail.com>
2025-12-10 15:58:52 +0000

    done day 4

--- 2025/day_4/test_day_4.py
+++ 2025/day_4/test_day_4.py
@@ -1,8 +1,25 @@
+import numpy as np
 import pytest
 
-import day_4
+from day_4 import get_true_neighbour_count, remove_rolls
 
 
-@pytest.mark.skip(reason="Day 4 not implemented yet")
-def test_placeholder():
-    pass
+@pytest.mark.parametrize("arr, expected", [
+    (np.array(
+        [
+            [0, 1, 0],
+            [1, 1, 0],
+            [0, 0, 1],
+        ]
+    ), np.array(
+        [
+            [3, 2, 2],
+            [2, 3, 3],
+            [2, 3, 1],
+        ]
+    )),
+    (np.array([[0, 1, 0]]), np.array([[1, 0, 1]]))
+])
+def test_true_neighbour_count(arr, expected):
+    """get_true_neighbour_count counts orthogonal neighbours"""
+    np.testing.assert_array_equal(get_true_neighbour_count(arr), expected)
