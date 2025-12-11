87de522764da420ae7bfab00808806bf7ab3fc8c
ayahusseini <aya.ahusseini@gmail.com>
2025-12-11 05:49:59 +0000

    day 5 testing

--- 2025/day_5/day_5.py
+++ 2025/day_5/day_5.py
@@ -4,25 +4,60 @@ INPUT_FILE = "inputs/day_5_input.txt"
 TEST_FILE = "inputs/day_5_test_input.txt"
 
 
-def load_file(filename: str):
+def load_file(filename: str) -> tuple[list, list]:
     """Load the puzzle input, yielding stripped lines."""
+    rngs = []
+    ids = []
     with open(filename, "r") as f:
         for line in f:
-            yield line.strip()
+            if '-' in line:
+                rng_text = line.strip().split('-')
+                rngs.append([int(rng_text[0]), int(rng_text[1])])
+            elif not line.strip():
+                continue
+            else:
+                ids.append(int(line.strip()))
+    return rngs, ids
+
+
+def merge_two_ranges(r1: list, r2: list, already_sorted=False) -> list[list]:
+    """Merge two inclusive ranges"""
+    if not already_sorted:
+        r1, r2 = sorted((r1, r2), key=lambda x: x[0])
+    if r2[0] > r1[1] + 1:
+        return [r1, r2]
+    s = min(r1[0], r2[0])
+    e = max(r1[1], r2[1])
+    return [[s, e]]
+
+
+def merge_all_ranges(ranges: list[list]) -> list[list]:
+    """Merge a list of inclusive ranges"""
+    if len(ranges) < 2:
+        return ranges
+
+    ranges = sorted(ranges, key=lambda x: x[0])
+    mutually_exclusive = [ranges[0]]
+    for right in ranges[1:]:
+        left = mutually_exclusive.pop()
+        mutually_exclusive += merge_two_ranges(left,
+                                               right, already_sorted=True)
+    return mutually_exclusive
 
 
 def one_star(filename: str):
     """Solve part 1 for day 5."""
-    raise NotImplementedError("Day 5 part 1 not implemented yet")
+    ranges, ids = load_file(filename)
+    return ranges, ids
 
 
 def two_star(filename: str):
     """Solve part 2 for day 5."""
-    raise NotImplementedError("Day 5 part 2 not implemented yet")
+    ranges, ids = load_file(filename)
 
 
 if __name__ == "__main__":
     print(f"One star test solution is {one_star(TEST_FILE)}")
-    print(f"Two star test solution is {two_star(TEST_FILE)}")
-    print(f"One star solution is {one_star(INPUT_FILE)}")
-    print(f"Two star solution is {two_star(INPUT_FILE)}")
+    # print(f"Two star test solution is {two_star(TEST_FILE)}")
+    # print(f"One star solution is {one_star(INPUT_FILE)}")
+    # print(f"Two star solution is {two_star(INPUT_FILE)}")
